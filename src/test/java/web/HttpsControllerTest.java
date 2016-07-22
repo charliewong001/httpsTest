2819f85d664a59ca77c6c29cdd3a5f27715bfbc0
charlie <1151832099@qq.com>
2016-07-22 16:40:36 +0800

    code init

---
+++ src/test/java/web/HttpsControllerTest.java
@@ -0,0 +1,37 @@
+package web;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import utils.HttpClientUtils;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:spring/spring-context.xml",
+        "classpath:spring/spring-mvc.xml" })
+public class HttpsControllerTest {
+
+    // @Test
+    public void testRequestByHttp() {
+        String url = "http://127.0.0.1:8080/httpsTest/http/request";
+        String param = "orderNo=123";
+        String result = HttpClientUtils.sendRequest("POST", url, "http", param,
+                "utf-8");
+        System.out.println(result);
+    }
+
+    @Test
+    public void testRequestByHttps() {
+        String url = "https://127.0.0.1:8443/httpsTest/https/request";
+        String param = "orderNo=123";
+        Map<String, String> data = new HashMap<String, String>();
+        String result = HttpClientUtils.sendRequestWithoutCer("POST", url,
+                "https", data, "utf-8");
+        System.out.println(result);
+    }
+
+}
